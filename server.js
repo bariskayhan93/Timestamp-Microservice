d9317d8a82e02f048eae9813251c292409f736bf
bariskayhan93 <bariskayhan53@gmail.com>
2021-12-12 00:55:21 +0100

    second test fcc

--- server.js
+++ server.js
@@ -2,57 +2,71 @@
 // where your node app starts
 
 // init project
-var express = require("express");
+var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
-var cors = require("cors");
-app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/views/index.html");
+  res.sendFile(__dirname + '/views/index.html');
 });
 
-// your first API endpoint...
+
+// your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({ greeting: "hello API" });
+  res.json({greeting: 'hello API'});
 });
-const date = new Date();
-app.get("/api/", (req, res) => {
+
+//return ip adress, language, software
+app.get("/api/whoami", (req,res)=>{
+res.json({
+  ipaddress:"ip",
+  language: "de-DE,de;q=0.9",
+  software: "Mozilla/5.0  Chrome/95.0.4638.69 Safari/537.36 OPR/81.0.4196.60"
+})
+})
+
+
+// return datum utc and unix 
+const date = new Date()
+app.get("/api/",(req,res)=>{
+res.json({
+  "unix":Date.now(),
+  "utc":date.toUTCString()
+})
+})
+
+app.get("/api/:date_string",(req,res)=>{
+let input=req.params.date_string;
+
+if(parseInt(input)>10000){
+  let unixTime=new Date(parseInt(input))
   res.json({
-    unix: Date.now(),
-    utc: date.toUTCString(),
-  });
-});
+    "unix":unixTime.getTime(),
+    "utc":unixTime.toUTCString()
+  })
+}
 
-app.get("/api/:date_string", (req, res) => {
-  let input = req.params.date_string;
+let passedValue=new Date(input)
 
-  if (parseInt(input) > 10000) {
-    let unixTime = new Date(parseInt(input));
-    res.json({
-      unix: unixTime.getTime(),
-      utc: unixTime.toUTCString(),
-    });
-  }
-  let passedValue = new Date(input);
+if(passedValue=="Invalid Date"){
+  res.json({ error : "Invalid Date" })
+}else{
+  res.json({
+    "unix":passedValue.getTime(),
+    "utc":passedValue.toUTCString(),input
+  })
+}})
 
-  if (passedValue == "Invalid Date") {
-    res.json({ error: "Invalid Date" });
-  } else {
-    res.json({
-      unix: passedValue.getTime(),
-      utc: passedValue.toUTCString(),
-    });
-  }
-});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-  console.log("Your app is listening on port " + listener.address().port);
+  console.log('Your app is listening on port ' + listener.address().port);
 });
