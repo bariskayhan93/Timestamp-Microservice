546957d72ea9fa95020a03e729f5c10f14e18527
bariskayhan93 <bariskayhan53@gmail.com>
2021-12-11 18:57:35 +0100

    utc

--- server.js
+++ server.js
@@ -2,51 +2,57 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
-var port=process.env.PORT||3000;
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: "hello API" });
+});
+const date = new Date();
+app.get("/api/", (req, res) => {
+  res.json({
+    unix: Date.now(),
+    utc: date.toUTCString(),
+  });
 });
 
-let resObj={}
-app.get("/api/timestamp/:date_string",(req,res)=>{
-let input=req.params.input;
-if(input.includes('-')){
-  resObj['unix']=new Date(input).getTime()
-  res.json({"r":"Invalid Date"})
-}else{
+app.get("/api/:date_string", (req, res) => {
+  let input = req.params.date_string;
 
+  if (parseInt(input) > 10000) {
+    let unixTime = new Date(parseInt(input));
+    res.json({
+      unix: unixTime.getTime(),
+      utc: unixTime.toUTCString(),
+    });
+  }
+  let passedValue = new Date(input);
 
-  res.json({"error":"Invalid Date"})
-}
-})
-
-
-
-
-
-
-
-
+  if (passedValue == "Invalid Date") {
+    res.json({ error: "Invalid Date" });
+  } else {
+    res.json({
+      unix: passedValue.getTime(),
+      utc: passedValue.toUTCString(),
+    });
+  }
+});
 
 // listen for requests :)
-var listener = app.listen(port, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+var listener = app.listen(process.env.PORT, function () {
+  console.log("Your app is listening on port " + listener.address().port);
 });
